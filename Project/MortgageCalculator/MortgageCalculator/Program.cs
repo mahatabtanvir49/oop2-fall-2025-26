6b32fcfff9618bf9a246133ab07941183ed30694
mahatabtanvir49 <mahatabtanvir518@gmail.com>
2025-11-05 14:41:17 +0600

    23-50106-1
    git commit -m 23-50106-1

---
+++ Project/MortgageCalculator/MortgageCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Mortage Calculator Bangladesh");
+
+double loanAmount;
+double interestRate;
+int loanPeriod;
+
+Console.Write("Enter loan amount: ");
+loanAmount = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter annual interest rate (in %): ");
+interestRate = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter loan period (in Years): ");
+loanPeriod = Convert.ToInt32(Console.ReadLine());
+
+
+double monthlyInterestRate = interestRate / 100 / 12;
+int totalPayment = loanPeriod * 12;
+
+double monthlyPayment = loanAmount * monthlyInterestRate * Math.Pow(1 + monthlyInterestRate, totalPayment) / (Math.Pow(1 + monthlyInterestRate, totalPayment) - 1);
+
+
+double totalPaid = monthlyPayment * totalPayment;
+double totalInterest = totalPaid - loanAmount;
+
+
+CultureInfo bdCulture = new CultureInfo("bn-BD");
+
+Console.WriteLine($"\nMonthly Payment: {monthlyPayment.ToString("C", bdCulture)}");
+Console.WriteLine($"\nTotal Paid: {totalPaid.ToString("C", bdCulture)}");
+Console.WriteLine($"\nTotal Interest: {totalInterest.ToString("C", bdCulture)}");
