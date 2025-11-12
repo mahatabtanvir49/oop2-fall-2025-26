5bff12ec1daccf3f7ff9339cdd227b3566d75342
mahatabtanvir49 <mahatabtanvir518@gmail.com>
2025-11-13 01:13:12 +0600

    23-50106-1

---
+++ Lab/HouseExercise/HouseExercise/Program.cs
@@ -0,0 +1,48 @@
+﻿class House
+{
+    public int BuildYear { get; set; }
+    public double Size { get; set; }
+
+    public House(int buildYear, double size)
+    {
+        this.BuildYear = buildYear;
+        this.Size = size;
+    }
+
+    private int HowOld()
+    {
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - BuildYear;
+        return age;
+    }
+
+    public bool CanBeSold()
+    {
+        int age = HowOld();
+        if (age > 15)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"House Built In: {BuildYear}");
+        Console.WriteLine($"Size: {Size} sq. ft.");
+        Console.WriteLine($"Can be sold: {CanBeSold()}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        House myHouse = new House(2003, 1107);
+        myHouse.ShowInfo();
+        Console.ReadLine();
+    }
+}
